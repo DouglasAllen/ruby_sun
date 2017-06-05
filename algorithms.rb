36ece45fd0552a1befb5209250a29863c32a7401
Douglas <kb9agt@gmail.com>
2017-06-05 11:11:28 -0500

    first commit

--- algorithms.rb
+++ algorithms.rb
@@ -23,7 +23,7 @@ def coefficients(jme, coeffs)
 end
 
 def horner(t, abc)
-  abc.each_with_index.reduce(0){ |a, (e, i)| a + e * t**i }
+  abc.each_with_index.reduce(0) { |a, (e, i)| a + e * t**i }
 end
 
 def heliocentric_longitude(jme)
@@ -52,6 +52,26 @@ def geocetric_latitude(hlat)
   -1 * hlat
 end
 
-def ma
-  [1287104.793048, 129596581.0481, - 0.5532, 0.000136, - 0.00001149]
+def ma(j)
+  a = [1_287_104.793048, 129_596_581.0481, - 0.5532, 0.000136, - 0.00001149]
+  horner(j * 10, a) / 3600 % 360
+end
+
+def ml(j)
+  a = [280.4664567, 36_000.76982779, 0.0003032028,
+       1.0 / 499_310.0, 1.0 / -152_990.0, 1.0 / -19_880_000.0]
+  horner(j * 10, a) % 360
+end
+
+def ec(g)
+  1.915 * Math.sin(g * Math::PI / 180) +
+    0.020 * Math.sin(2 * g * Math::PI / 180)
+end
+
+def tl(j)
+  (ec(ma(j)) + ml(j)) % 360
+end
+
+def om
+  [450_160.398036, - 6_962_890.5431, 7.4722, 0.007702, - 0.00005939]
 end
--- test.rb
+++ test.rb
@@ -14,7 +14,9 @@ glat = geocetric_latitude(hlat)
 
 esau = astronomical_units(jme)
 
-mean_anomaly = horner(jme * 10, ma) / 3600 % 360
+mean_anomaly = ma(jme)
+mean_longitude = ml(jme)
+true_longitude = tl(jme)
 
 puts "Date #{date.to_time}"
 puts "heliocentric longitude Earth #{hlon * 180 / Math::PI}"
@@ -23,18 +25,5 @@ puts "heliocentric latitude Earth #{hlat * 180 / Math::PI}"
 puts "geocentric latitude Sun #{glat * 180 / Math::PI}"
 puts "Earth Sun astronomical units #{esau}"
 puts "mean anomaly Sun #{mean_anomaly}"
-
-# http://aa.usno.navy.mil/faq/docs/SunApprox.php
-
-d = date.ajd.to_f - 2_451_545.0
-g = mean_anomaly
-q = 280.46645 + 0.98564736 * d
-ec = (
-  1.915 * Math.sin(
-    g * Math::PI / 180
-  )) + 0.020 * Math.sin(
-    2 * g * Math::PI / 180
-  )
-l = q + ec
-puts "mean longitude Sun #{q % 360}"
-puts "true longitude Sun #{l % 360}"
+puts "mean longitude Sun #{mean_longitude}"
+puts "true longitude Sun #{true_longitude}"
